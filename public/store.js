fbffe5c8a6bf7a942271a1649f8510980541ccaf
Zach <lo@Zachs-MacBook-Pro.local>
2025-12-20 13:40:50 -0500

    json parse, save and retrieve is working

--- public/store.js
+++ public/store.js
@@ -1,68 +1,47 @@
 "use strict";
 
 document.addEventListener("DOMContentLoaded", function () {
-  // Function to load saved recipes from localStorage
-  function loadSavedRecipes() {
-    const savedRecipes = JSON.parse(localStorage.getItem("savedRecipes")) || [];
-    savedRecipes.forEach((recipe) => {
-      const recipeElement = document.createElement("article");
-      recipeElement.classList.add("recipe-card");
-      recipeElement.innerHTML = `
-      <a href=${recipe.url} class="recipe-card-link">
-        <div class="recipe-card-outer">
-          <img src=${recipe.image} alt=${recipe.alt} class="recipe-card-image"/>
-          <div class="recipe-card-inner">
-                  <span class="recipe-subtitle">${recipe.subtitle}</span>
-                  <h1 class="recipe-title">${recipe.title}</h1>
-          </div>
-        </div>
-      </a>
-      `;
-
-      savedRecipesContainer.appendChild(recipeElement);
-    });
-  }
-
-  // Load saved recipes on page load
-  loadSavedRecipes();
-
+  // Save button functionality
   const saveButton = document.getElementById("save-button");
-  const savedRecipesContainer = document.getElementById("recipe-cards");
 
   if (saveButton) {
     saveButton.addEventListener("click", function () {
-      alert("Recipe saved!"); // works
       const recipePath = window.location.pathname;
-      console.log("Current URL:", recipePath);
+
       const recipeTitle = document.querySelector(
         "#recipe-introduction h1"
       ).textContent;
       const recipeSubtitle = document.querySelector(
         "#recipe-introduction .subtitle"
       ).textContent;
-      const recipeImageSrc = document.querySelector(
-        "#recipe-introduction img"
-      ).src;
-      const recipeImageAlt = document.querySelector(
-        "#recipe-introduction img"
-      ).alt;
-      console.log("Recipe title:", recipeTitle);
-      console.log("Recipe subtitle:", recipeSubtitle);
-      console.log("Recipe image src:", recipeImageSrc);
-      console.log("Recipe image alt:", recipeImageAlt);
-      console.log("Recipe path:", recipePath);
+      const recipeImageSrc = document
+        .querySelector("#recipe-introduction figure img")
+        .getAttribute("src");
+      const recipeImageAlt = document
+        .querySelector("#recipe-introduction figure img")
+        .getAttribute("alt");
+      // console.log("Recipe title:", recipeTitle);
+      // console.log("Recipe subtitle:", recipeSubtitle);
+      // console.log("Recipe image src:", recipeImageSrc);
+      // console.log("Recipe image alt:", recipeImageAlt);
+      // console.log("Recipe path:", recipePath);
+
       // Create recipe object
       const recipe = {
-        image: recipeImageSrc,
-        alt: recipeImageAlt,
-        url: recipePath,
+        id: Date.now(),
         title: recipeTitle,
         subtitle: recipeSubtitle,
+        imageSrc: recipeImageSrc,
+        imageAlt: recipeImageAlt,
+        path: recipePath,
       };
+
+      // Save and retrieve recipes to the savedRecipes array in user's browser local storage
       let savedRecipes = JSON.parse(localStorage.getItem("savedRecipes")) || [];
       savedRecipes.push(recipe);
       localStorage.setItem("savedRecipes", JSON.stringify(savedRecipes));
-      loadSavedRecipes();
+
+      alert("Recipe saved!");
     });
   }
 });
